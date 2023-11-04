e1b830e7b6d28bb25a580af980a9132cb545750f
Mahmudur Rahman Salman <mahmudur.salman@gmail.com>
2023-11-04 23:10:34 +0600

    form validation added

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-let form = document.getElementById('form'); 
-let textInput = document.getElementById('textInput'); 
-let dateInput = document.getElementById('dateInput'); 
-let textArea = document.getElementById('textarea'); 
-let msg = document.getElementById('msg'); 
-let tasks = document.getElementById('tasks'); 
-let add = document.getElementById('add'); 
+let form = document.getElementById("form");
+let textInput = document.getElementById("textInput");
+let dateInput = document.getElementById("dateInput");
+let textArea = document.getElementById("textarea");
+let msg = document.getElementById("msg");
+let tasks = document.getElementById("tasks");
+let add = document.getElementById("add");
+
+// form validations
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
+
+let formValidation = () => {
+    if(textInput.value === ''){
+        console.log('failure');
+        msg.innerHTML = "Task input box cannot be empty"; 
+    }
+    else{
+        console.log('success'); 
+        msg.innerHTML = ""; 
+    }
+}
+ 
